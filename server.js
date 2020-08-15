0d34cc6126cf1eb96286cc8a6ea3604d7df9f04c
unknown <caturilham05@gmail.com>
2020-08-15 17:05:27 +0700

    instalasi cors origin di backend

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const Cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(Cors());
 
 //panggil router
 const routes = require('./router');
@@ -15,6 +17,6 @@ routes(app);
 //daftarkan menu routes dari index
 app.use('/auth', require('./middleware'));
 
-app.listen(3000, () => {
-    console.log(`Server started on port 3000 `);
+app.listen(3001, () => {
+    console.log(`Server started on port ${3001}`);
 });
